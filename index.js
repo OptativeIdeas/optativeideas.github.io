86aff032e7f47b0ef278f6f9fe35aadb91edbad6
OptativeIdeas <83053813+OptativeIdeas@users.noreply.github.com>
2023-03-08 20:29:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -56,7 +56,28 @@ navClose.addEventListener("click", () => {
 
 // Colors
 
+const widget = document.querySelector(".widget");
+const control = document.querySelector(".control");
 
+widget.addEventListener("click", () => {
+  control.classList.toggle("open");
+});
+
+const colors = [...document.querySelectorAll(".colors span")];
+document.querySelector(":root").style.setProperty("--customColor", "#0044ff");
+
+colors.forEach((color) => {
+  color.addEventListener("click", () => {
+    const currentColor = color.dataset.id;
+    document
+      .querySelector(":root")
+      .style.setProperty("--customColor", currentColor);
+  });
+});
+
+window.addEventListener("scroll", () => {
+  control.classList.remove("open");
+});
 
 // Glidejs
 
